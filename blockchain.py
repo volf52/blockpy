25a1bf814282522eb98e49195f6c8e6f77b7bc8a
Muhammad Arslan <rslnkrmt2552@gmail.com>
2018-01-16 20:13:22 +0300

    Done for now?

--- blockchain.py
+++ blockchain.py
@@ -8,17 +8,20 @@ A simple implementation of Blockchain in Python.
 
 import hashlib
 import json
+import requests
 
 from time import time
 from uuid import uuid4
 from textwrap import dedent
 from flask import Flask, jsonify, request
+from urllib.parse import urlparse
 
 class Blockchain(object):
     """ A Blockchain object with a list of transactions and blocks. """
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         # Create a genesis block.
         self.new_block(previous_hash=1, proof=100)
@@ -78,6 +81,79 @@ class Blockchain(object):
             proof += 1
         
         return proof
+    
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes.
+
+        :param address: <str> Address of the node e.g. 'http://192.168.0.5:5000'
+        :return: None
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid.
+        
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False otherwise
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-------------------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+        
+        return True
+
+    def resolve_conflicts(self):
+        """
+        Our Consensus algorithm.
+        
+        Resolves conflicts by replacing our chain with the longest one in the network.
+
+        :return: <bool> True if our chain was replaces, False otherwise
+        """
+
+        neighbors = self.nodes
+        new_chain = None
+
+        max_len = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network.
+        for node in neighbors:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if length is longer than our chain length, and chain is valid.
+                if length > max_len and self.valid_chain(chain):
+                    max_len = len(chain)
+                    new_chain = chain
+            
+            if new_chain:
+                self.chain = new_chain
+                return True
+            return False
+
+
 
     @staticmethod
     def valid_proof(last_proof, proof):
@@ -170,6 +246,41 @@ def full_chain():
         'length' : len(blockchain.chain),
     }
     return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes.", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+    
+    response = {
+        'message' : 'New nodes have been added',
+        'total_nodes' : list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaces:
+        response = {
+            'message' : 'Our chain was replaced',
+            'new_chain' : blockchain.chain
+        }
+    else:
+        response = {
+            'message' : 'Our chain is authoritative', 
+            'chain' : blockchain.chain
+        }
+    
+    return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
