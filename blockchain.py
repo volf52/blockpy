a5a43078f29080ceeba6961e27f19a0e9381780f
Muhammad Arslan <rslnkrmt2552@gmail.com>
2018-01-16 18:48:18 +0300

    Basic PoW concept.
    Check if hash of pp' contains leading 4 zeroes, where p' is the proof and p is the proof for previous block.

--- blockchain.py
+++ blockchain.py
@@ -8,8 +8,9 @@ A simple implementation of Blockchain in Python.
 
 import hashlib
 import json
-from time import time
 
+from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     """ A Blockchain object with a list of transactions and blocks. """
@@ -59,6 +60,37 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+            - Find a number p' such that the hash(pp') contains leading 4 zeroes, where p is the previous p'
+            - p is the previous proof, and p' is the new proof
+
+        
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while not self.valid_proof(last_proof, proof):
+            proof += 1
+        
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False otherwise
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
     @staticmethod
     def hash(block):
         """ 
